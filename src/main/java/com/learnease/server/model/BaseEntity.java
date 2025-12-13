fbb89a7960f35dba27113e2665dfe24d858c4f09
Swarajsr1234 <swarajrawate28@gmail.com>
2025-12-13 15:50:53 +0530

    Base entity

---
+++ src/main/java/com/learnease/server/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.learnease.server.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
