5a484bd7afe656de7248fd4ac6e8fab0267cba33
Swarajsr1234 <swarajrawate28@gmail.com>
2026-01-21 13:10:45 +0530

    feat(security): permit OPTIONS requests for CORS preflight

--- src/main/java/com/learnease/server/config/SecurityConfig.java
+++ src/main/java/com/learnease/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.learnease.server.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -51,8 +52,10 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/user/auth/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 )
+
                 .addFilterBefore(
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class
