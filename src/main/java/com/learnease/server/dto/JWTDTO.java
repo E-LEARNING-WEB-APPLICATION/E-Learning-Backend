ddb9383d34a09b370bb5462a25d62728bdabc95b
Swarajsr1234 <swarajrawate28@gmail.com>
2026-01-22 13:44:49 +0530

    fix(auth): store UUID as string in JWT and parse during authentication

--- src/main/java/com/learnease/server/dto/JWTDTO.java
+++ src/main/java/com/learnease/server/dto/JWTDTO.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class JWTDTO {
-    private Long userId;
+    private UUID userId;
     private String email;
     private String role;
 }
