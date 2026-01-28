72e6dec3619ffeee2ae6f71d3eef4e69a7f61475
Omkar Gandhi <gandhioms16@gmail.com>
2026-01-28 16:40:30 +0530

    fix: added PATCH method to cors config

--- src/main/java/com/learnease/server/config/SecurityConfig.java
+++ src/main/java/com/learnease/server/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
