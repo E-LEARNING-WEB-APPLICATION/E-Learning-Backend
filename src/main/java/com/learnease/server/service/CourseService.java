fb4d3439bcc6f0ecefaf7ffb99702c1497062964
Sanket Sudhakar Raut <111580160+Sanketr113@users.noreply.github.com>
2026-01-30 11:17:23 +0530

    feat(StudentDashboard): add explore course page with all courses available with help of pagination

--- src/main/java/com/learnease/server/service/CourseService.java
+++ src/main/java/com/learnease/server/service/CourseService.java
@@ -2,8 +2,11 @@ package com.learnease.server.service;
 
 import com.learnease.server.dto.ApiResponse;
 import com.learnease.server.dto.course.CourseResponseDto;
+import com.learnease.server.dto.course.DashboardCourseFilterDto;
 import com.learnease.server.dto.course.DashboardCoursesResponseDto;
 import com.learnease.server.dto.course.EnrolledCourseResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,4 +23,6 @@ public interface CourseService {
 
     public List<EnrolledCourseResponseDto> getMyEnrolledCourses(UUID authId);
     long getCourseCount();
+
+    Page<DashboardCoursesResponseDto> getDashboardCourses(DashboardCourseFilterDto filter, Pageable pageable);
 }
