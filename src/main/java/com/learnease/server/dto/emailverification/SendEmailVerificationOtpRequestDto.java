a27b965df9763f50b42ab9338adb0746de90f9a8
Swarajsr1234 <swarajrawate28@gmail.com>
2026-01-30 21:26:53 +0530

    feat(auth): add email verification OTP enums and request DTOs

---
+++ src/main/java/com/learnease/server/dto/emailverification/SendEmailVerificationOtpRequestDto.java
@@ -0,0 +1,16 @@
+package com.learnease.server.dto.emailverification;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SendEmailVerificationOtpRequestDto {
+
+    @Email
+    @NotBlank
+    private String email;
+}
